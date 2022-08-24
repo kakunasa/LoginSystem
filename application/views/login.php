d4a3194c437a3962b84d0abfb645966ffac979e9
guo <weican.guo@coinbest.com>
2022-08-24 17:56:57 +0900

    临时备注

--- application/views/login.php
+++ application/views/login.php
@@ -29,3 +29,46 @@
 </form>
 </body>
 </html>
+
+
+post 异步JS提交
+
+
+
+
+mysql curd 常见的查询语句
+
+where 多条件 and or
+where in
+has
+join left/right join
+去重查询
+count
+max min
+top
+order by
+limit offset
+group by 分类汇总 sum 加减乘除 平均值
+时间戳互转时间
+union
+
+
+事务
+
+表的概念
+视图概念
+
+
+
+
+
+php基本语法 常用函数 class定义
+
+oob
+继承 封装 多态
+
+接口类 抽象类 继承父类
+
+
+
+
