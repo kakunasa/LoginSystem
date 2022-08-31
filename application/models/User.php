79ca7afe7f67355fb58c5db37b50b82dfc8b3a65
kakunasa <94429891+kakunasa@users.noreply.github.com>
2022-08-31 18:47:53 +0900

    Add files via upload
    mysql事务

--- application/models/User.php
+++ application/models/User.php
@@ -22,6 +22,22 @@ class User extends CI_Model {
 		return $this->db->where('username',$username)->get('tbl_user')->row_array();
 	}
 
+	public function is_user_exist($username){
+		$query = $this->db->where('username',$username)->get('tbl_user')->row_array();
+		if($query){
+			return TRUE;
+		}
+		return FALSE;
+	}
+
+public function is_user_info_exist($username){
+	$query = $this->db->where('username',$username)->get('tbl_user_info')->row_array();
+	if($query){
+		return TRUE;
+	}
+	return FALSE;
+}
+
 	public function search_user_info($username){
 		return $this->db->where('username',$username)->get('tbl_user_info')->row_array();
 	}
@@ -32,24 +48,34 @@ class User extends CI_Model {
 			'username' => $username,
 			'password'  => $password,
 		);
-		$is_success = $this->db->insert('tbl_user',$data);
-		if ($is_success){
-			return $this->db->insert_id();
-		}
-		return 0;
-	}
 
-	public function insert_user_info($user_info_id)
-	{
-		$data = array(
-			'user_id' => $username,
-			'password'  => $password,
-		);
-		$is_success = $this->db->insert('tbl_user',$data);
-		if ($is_success){
+		$this->db->insert('tbl_user',$data);
+		if ($this->db->affected_rows() == 1) {
 			return $this->db->insert_id();
 		}else{
-			return 'insert failed';
+			return 0;
+		}
+	}
+
+	public function get_user_id($username){
+		$this -> db -> select('user_id');
+		$user_id = $this -> db -> get_where ('tbl_user',array('username'=>$username),0,1) -> row_array();
+		return (int)$user_id['user_id'];
+	}
+
+	public function insert_user_info($username,$userid,$firstname,$lastname)
+	{
+		$data_info = array(
+			'username' => $username,
+			'first_name' => $firstname,
+			'last_name' => $lastname,
+			'user_id' => $userid,
+		);
+		$this->db->insert('tbl_user_info',$data_info);
+		if ($this->db->affected_rows() == 1) {
+			return $this->db->insert_id();
+		}else{
+			return 0;
 		}
 	}
 
@@ -65,5 +91,8 @@ class User extends CI_Model {
 
 	public function delete_user($username){
 		return $this->db->delete('tbl_user', array('username' => $username));
+	}
+	public function delete_user_info($username){
+		return $this->db->delete('tbl_user_info', array('username' => $username));
 	}
 }
