4ec5e7b851ee7ca1a2b6c4eeaa88e2a2585a3204
kakunasa <94429891+kakunasa@users.noreply.github.com>
2022-08-31 18:48:33 +0900

    Add files via upload
    mysql事务

--- application/views/login.php
+++ application/views/login.php
@@ -27,59 +27,19 @@
 	<p>username:<input type='email' name='username' required/></p>
 	<p><input type='submit'/>search</p>
 </form>
+<span>***************************************</span>
+<form method='post' action='/Login/signup_user'>
+	<p>username:<input type='email' name='username' required/></p>
+	<p>password:<input type='text' name='password' required/></p>
+	<p>firstname:<input type='text' name='firstname' required/></p>
+	<p>lastname:<input type='text' name='lastname' required/></p>
+	<p><input type='submit'/>sing up</p>
+</form>
 </body>
 </html>
 
 
 post 异步JS提交
-
-
-
-
-mysql curd 常见的查询语句
-
-where 多条件 and or
-where in
-has
-join left/right join
-去重查询
-count
-max min
-top
-order by
-limit offset
-group by 分类汇总 sum 加减乘除 平均值
-时间戳互转时间
-union
-
-SELECT password , count(password) FROM tbl_user GROUP BY password;
-
-select password ,user_id from tbl_user where password='password' limit 1;
-
-select `password` , count(password) as c,any_value(`created_time`) as aaa from tbl_user GROUP BY `password`;
-
-select `password` , count(password) as c,any_value(`user_id`) as user_id from tbl_user GROUP BY `password`;
-
-select tbl_user.username as aaa,tbl_user_info.username as bbb ,tbl_user.password as pwd from tbl_user left join tbl_user_info on tbl_user.username = tbl_user_info.username;
-
-select a.username as aaa,b.username as bbb ,a.password as pwd from tbl_user a right join tbl_user_info b on a.username = b.username;
-
-select a.username as aaa,b.username as bbb ,a.password as pwd from tbl_user a inner join tbl_user_info b on a.username = b.username;
-
-select a.username as aaa,b.username as bbb ,a.password as pwd from tbl_user a join tbl_user_info b on a.username = b.username;
-
-update tbl_user set password='123456' , created_time=current_timestamp where user_id=8;
-
-事务
-
-表的概念
-视图概念
-
-锁机制
-
-
-
-
 
 php基本语法 常用函数 class定义
 
